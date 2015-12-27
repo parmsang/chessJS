c698e9312b1dd2bfe5f7c573c1e06589aa23601d
Ajitsy <sanghera.p@gmail.com>
2015-12-27 21:02:51 +0000

    set up GameBoard object with castling rules

---
+++ js/board.js
@@ -0,0 +1,8 @@
+var GameBoard = {};
+
+GameBoard.pieces = [BRD_SQ_NUM];
+GameBoard.side = COLOUR.WHITE;
+GameBoard.fiftyMove = 0;
+GameBoard.hisPly = 0;
+GameBoard.ply = 0;
+GameBoard.castlePerm = 0;
