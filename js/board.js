087f90fffadf3ae13bb16a31cc813746027aa1f2
Ajitsy <sanghera.p@gmail.com>
2015-12-30 16:38:44 +0000

    setting up FEN position string

--- js/board.js
+++ js/board.js
@@ -81,8 +81,66 @@ function ResetBoard() {
 
 }
 
+//rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
+
 function ParseFen(fen) {
 
-  ResetBoard();
+	ResetBoard();
+
+	var rank = RANKS.RANK_8;
+    var file = FILES.FILE_A;
+    var piece = 0;
+    var count = 0;
+    var i = 0;
+	var sq120 = 0;
+	var fenCnt = 0; // fen[fenCnt]
+
+	while ((rank >= RANKS.RANK_1) && fenCnt < fen.length) {
+	    count = 1;
+		switch (fen[fenCnt]) {
+			case 'p': piece = PIECES.bP; break;
+            case 'r': piece = PIECES.bR; break;
+            case 'n': piece = PIECES.bN; break;
+            case 'b': piece = PIECES.bB; break;
+            case 'k': piece = PIECES.bK; break;
+            case 'q': piece = PIECES.bQ; break;
+            case 'P': piece = PIECES.wP; break;
+            case 'R': piece = PIECES.wR; break;
+            case 'N': piece = PIECES.wN; break;
+            case 'B': piece = PIECES.wB; break;
+            case 'K': piece = PIECES.wK; break;
+            case 'Q': piece = PIECES.wQ; break;
+
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+                piece = PIECES.EMPTY;
+                count = Number(fen[fenCnt]);
+                break;
+
+            case '/':
+            case ' ':
+                rank--;
+                file = FILES.FILE_A;
+                fenCnt++;
+                continue;
+            default:
+                console.log("FEN error");
+                return;
+
+		}
+
+		for (i = 0; i < count; i++) {
+			sq120 = FR2SQ(file,rank);
+      GameBoard.pieces[sq120] = piece;
+			file++;
+        }
+		fenCnt++;
+	}
 
 }
