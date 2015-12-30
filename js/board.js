34ec3ea97e3023df4e3b4f8e31b252541b3cd012
Ajitsy <sanghera.p@gmail.com>
2015-12-30 15:55:07 +0000

    added position key hashes

--- js/board.js
+++ js/board.js
@@ -1,8 +1,43 @@
+function PCEINDEX (pce, pceNum) {
+  return (pce * 10 + pceNum);
+}
+
 var GameBoard = {};
 
 GameBoard.pieces = [BRD_SQ_NUM];
-GameBoard.side = COLOUR.WHITE;
+GameBoard.side = COLOURS.WHITE;
 GameBoard.fiftyMove = 0;
 GameBoard.hisPly = 0;
 GameBoard.ply = 0;
+GameBoard.enPas = 0;
 GameBoard.castlePerm = 0;
+GameBoard.material = new Array(2);
+GameBoard.pceNum = new Array (13);
+GameBoard.pList = new Array(14 * 10);
+GameBoard.posKey = 0;
+
+function GeneratePosKey() {
+  var sq = 0;
+  var finalKey;
+  var piece = PIECES.EMPTY;
+
+  for (sq = 0; sq < BRD_SQ_NUM; ++sq) {
+    piece = GameBoard.pieces[sq];
+    if(piece != PIECES.EMPTY && piece != SQUARES.OFFBOARD) {
+      finalKey ^= PieceKeys[(piece * 120) + sq];
+    }
+  }
+
+  if(GameBoard.side == COLOURS.WHITE) {
+    finalKey ^= SideKey;
+  }
+
+  if(GameBoard.enPas != SQUARES.NO_SQ) {
+    finalKey ^= PieceKeys[GameBoard.enPas];
+  }
+
+  finalKey ^= CastleKeys[GameBoard.castlePerm];
+
+  return finalKey;
+
+}
--- js/main.js
+++ js/main.js
@@ -2,6 +2,24 @@ $(function() {
   init();
   console.log("Main Init called");
 
+  var piece1 = RAND_32();
+  var piece2 = RAND_32();
+  var piece3 = RAND_32();
+  var piece4 = RAND_32();
+
+  var key = 0;
+  key ^= piece1;
+  key ^= piece2;
+  key ^= piece3;
+  key ^= piece4;
+  console.log("key:" + key.toString(16));
+  key = 0;
+  key ^= piece4;
+  key ^= piece2;
+  key ^= piece1;
+  key ^= piece3;
+  console.log("key:" + key.toString(16));
+
 });
 
 function InitFilesRanksBrd() {
@@ -30,7 +48,48 @@ function InitFilesRanksBrd() {
 
 }
 
+function InitSq120To64() {
+
+  var index = 0;
+  var file = FILE.FILE_A;
+  var rank = RANK.RANK_1;
+  var sq = SQUARES.A1;
+  var sq64 = 0;
+
+  for (index = 0; index < BRD_SQ_NUM; ++index) {
+    Sq120ToSq64[index] = 65;
+  }
+
+  for (index = 0; index < 64; ++index) {
+    Sq64ToSq120[index] = 120;
+  }
+
+  for(rank = RANKS.RANK_1; rank <= RANKS.RANK_8; ++rank) {
+    for(file = FILES.FILE_A; file <= FILES.FILE_H; ++file) {
+      sq = FR2SQ(file, rank);
+      Sq64ToSq120[sq64] = sq;
+      Sq120ToSq64[sq] = sq64;
+      sq64++;
+    }
+  }
+}
+
+function InitHashKeys() {
+    var index = 0;
+
+	for(index = 0; index < 14 * 120; ++index) {
+		PieceKeys[index] = RAND_32();
+	}
+
+	SideKey = RAND_32();
+
+	for(index = 0; index < 16; ++index) {
+		CastleKeys[index] = RAND_32();
+	}
+}
+
 function init() {
   console.log("init() called");
   InitFilesRanksBrd();
+  InitHashKeys();
 }
