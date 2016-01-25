f1c9edcd5540dc930569ea8db15be8a48451a835
Ajitsy <sanghera.p@gmail.com>
2016-01-25 20:58:59 +0000

    added code for generating castling moves

--- js/movegen.js
+++ js/movegen.js
@@ -4,72 +4,106 @@ function MOVE(from, to, captured, promoted, flag) {
 
 function GenerateMoves() {
 
-  GameBoard.moveListStart[GameBoard.ply+1] = GameBoard.moveListStart[GameBoard.ply];
+  GameBoard.moveListStart[GameBoard.ply + 1] = GameBoard.moveListStart[GameBoard.ply];
 
   var pceType;
   var pceNum;
   var sq;
 
-  if(GameBoard.side == COLOURS.WHITE) {
+  if (GameBoard.side == COLOURS.WHITE) {
     pceType = PIECES.wP;
 
-    for(pceNum = 0; pceNum < GameBoard.pceNum[pceType]; ++pceType) {
+    for (pceNum = 0; pceNum < GameBoard.pceNum[pceType]; ++pceType) {
       sq = GameBoard.pList[PCEINDEX(pceType, pceNum)];
 
-      if(GameBoard.pece[sq+10] == PICES.EMPTY) {
+      if (GameBoard.pece[sq + 10] == PICES.EMPTY) {
         // add pawn move
-        if(RanksBrd[sq] == RANKS.RANK_2 && GameBoard.pieces[sq + 20] == PIECES.EMPTY) {
+        if (RanksBrd[sq] == RANKS.RANK_2 && GameBoard.pieces[sq + 20] == PIECES.EMPTY) {
           // Add quiet move
         }
       }
-      if(SQOFFBOARD(sq + 9) == BOOL.FALSE && PieceCol[GameBoard.pieces[sq+9]] == COLOURS.BLACK) {
+      if (SQOFFBOARD(sq + 9) == BOOL.FALSE && PieceCol[GameBoard.pieces[sq + 9]] == COLOURS.BLACK) {
         // add pawn cap move
       }
 
-      if(SQOFFBOARD(sq + 9) == BOOL.FALSE && PieceCol[GameBoard.pieces[sq+11]] == COLOURS.BLACK) {
+      if (SQOFFBOARD(sq + 9) == BOOL.FALSE && PieceCol[GameBoard.pieces[sq + 11]] == COLOURS.BLACK) {
         // add pawn cap move
       }
       if (GameBoard.enPas != SQUARES.NOSQ) {
-        if(sq +9 == GameBoard.enPas) {
+        if (sq + 9 == GameBoard.enPas) {
           // add en passsant move
         }
-        if(sq +11 == GameBoard.enPas) {
+        if (sq + 11 == GameBoard.enPas) {
+          // add en passsant move
+        }
+      }
+
+    }
+
+    if (GameBoard.castlePerm & CASTLEBIT.WKCA) {
+      if (GameBoard.pieces[SQUARES.F1] == PIECES.EMPTY && GameBoard.pieces[SQUARES.G1] == PIECES.EMPTY) {
+        if (SqAttacked(SQUARES.F1, COLOURS.BLACK) == BOOL.FALSE && SqAttacked(SQUARES.E1, COLOURS.BLACK) == BOOL.FALSE) {
+          // add quiet move
+        }
+      }
+    }
+
+    if (GameBoard.castlePerm & CASTLEBIT.WQCA) {
+      if (GameBoard.pieces[SQUARES.D1] == PIECES.EMPTY && GameBoard.pieces[SQUARES.C1] == PIECES.EMPTY && GameBoard.pieces[SQUARES.B1] == PIECES.EMPTY) {
+        if (SqAttacked(SQUARES.D1, COLOURS.BLACK) == BOOL.FALSE && SqAttacked(SQUARES.E1, COLOURS.BLACK) == BOOL.FALSE) {
+          // add quiet move
+        }
+      }
+    }
+
+
+    pceType = PIECE.wN;
+  } else {
+    pceType = PIECES.bP;
+
+    for (pceNum = 0; pceNum < GameBoard.pceNum[pceType]; ++pceType) {
+      sq = GameBoard.pList[PCEINDEX(pceType, pceNum)];
+
+      if (GameBoard.pece[sq - 10] == PICES.EMPTY) {
+        // add pawn move
+        if (RanksBrd[sq] == RANKS.RANK_2 && GameBoard.pieces[sq - 20] == PIECES.EMPTY) {
+          // Add quiet move
+        }
+      }
+      if (SQOFFBOARD(sq - 9) == BOOL.FALSE && PieceCol[GameBoard.pieces[sq - 9]] == COLOURS.WHITE) {
+        // add pawn cap move
+      }
+
+      if (SQOFFBOARD(sq - 11) == BOOL.FALSE && PieceCol[GameBoard.pieces[sq - 11]] == COLOURS.WHITE) {
+        // add pawn cap move
+      }
+      if (GameBoard.enPas != SQUARES.NOSQ) {
+        if (sq - 9 == GameBoard.enPas) {
+          // add en passsant move
+        }
+        if (sq - 11 == GameBoard.enPas) {
           // add en passsant move
         }
       }
     }
 
-    pceType = PIECE.wN;
-    } else {
-      pceType = PIECES.bP;
+    if (GameBoard.castlePerm & CASTLEBIT.BKCA) {
+      if (GameBoard.pieces[SQUARES.F8] == PIECES.EMPTY && GameBoard.pieces[SQUARES.G8] == PIECES.EMPTY) {
+        if (SqAttacked(SQUARES.F8, COLOURS.BLACK) == BOOL.FALSE && SqAttacked(SQUARES.E8, COLOURS.BLACK) == BOOL.FALSE) {
+          // add quiet move
+        }
+      }
+    }
 
-          for(pceNum = 0; pceNum < GameBoard.pceNum[pceType]; ++pceType) {
-            sq = GameBoard.pList[PCEINDEX(pceType, pceNum)];
+    if (GameBoard.castlePerm & CASTLEBIT.BQCA) {
+      if (GameBoard.pieces[SQUARES.D8] == PIECES.EMPTY && GameBoard.pieces[SQUARES.C8] == PIECES.EMPTY && GameBoard.pieces[SQUARES.B8] == PIECES.EMPTY) {
+        if (SqAttacked(SQUARES.D8, COLOURS.BLACK) == BOOL.FALSE && SqAttacked(SQUARES.E8, COLOURS.BLACK) == BOOL.FALSE) {
+          // add quiet move
+        }
+      }
+    }
 
-            if(GameBoard.pece[sq-10] == PICES.EMPTY) {
-              // add pawn move
-              if(RanksBrd[sq] == RANKS.RANK_2 && GameBoard.pieces[sq - 20] == PIECES.EMPTY) {
-                // Add quiet move
-              }
-            }
-            if(SQOFFBOARD(sq - 9) == BOOL.FALSE && PieceCol[GameBoard.pieces[sq-9]] == COLOURS.WHITE) {
-              // add pawn cap move
-            }
-
-            if(SQOFFBOARD(sq - 11) == BOOL.FALSE && PieceCol[GameBoard.pieces[sq-11]] == COLOURS.WHITE) {
-              // add pawn cap move
-            }
-            if (GameBoard.enPas != SQUARES.NOSQ) {
-              if(sq - 9 == GameBoard.enPas) {
-                // add en passsant move
-              }
-              if(sq - 11 == GameBoard.enPas) {
-                // add en passsant move
-              }
-            }
-          }
-
-      pceType = PIECE.bN;
+    pceType = PIECE.bN;
   }
 
 }
